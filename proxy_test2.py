6b6fe6b5ddc7073ef6fb79d5a2a8dfd614d18919
wohnyjalker <wohnyjalker@gmail.com>
2019-10-30 15:44:39 +0100

    add proxy_test2.py

--- proxy_test2.py
+++ proxy_test2.py
@@ -1,16 +1,56 @@
 import requests
+from concurrent.futures import ProcessPoolExecutor
+from user_agent import generate_user_agent
 
-url = 'http://api.ipify.org?format=json'
 
-r = requests.get(url)
+def make_session():
+    headers = {
+        'User-Agent': generate_user_agent()
+    }
+    with requests.Session() as s:
+        s.headers.update(**headers)
+    return s
 
-print(r.json())
 
-proxies = {
-    'http': 'http://95.209.155.91:8080',
-    'https': 'http://95.209.155.91:8080',
-}
+def build_proxies_list():
+    http = 'http://{}'
 
-r = requests.get(url=url, proxies=proxies)
+    proxy_list = list()
+    with open('proxylist', 'r') as f:
+        for ip in f:
+            proxy_list.append({
+                'http': http.format(ip.strip()),
+                'https': http.format(ip.strip())
+            })
+    return proxy_list
 
-print(r.json())
+
+def get_ip(proxies):
+    url = 'http://api.ipify.org?format=json'
+    session = make_session()
+    # r = session.get(url)
+    # print(r.json())
+    try:
+        r = session.get(url=url, proxies=proxies)
+        print(r.json())
+    except requests.exceptions.ProxyError as e:
+        print(e)
+        pass
+    finally:
+        return True
+
+
+def run_executor():
+    proxy_list = build_proxies_list()
+    print(proxy_list)
+    with ProcessPoolExecutor(max_workers=10) as executor:
+        executor.map(get_ip, proxy_list)
+
+
+def run():
+    proxy_list = build_proxies_list()
+    for proxy in proxy_list:
+        get_ip(proxy)
+
+
+run_executor()
